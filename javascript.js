7845d6c4c9edb6bb5914c788a85436598af86353
Adam Harrison <adamdharrison@gmail.com>
2021-12-11 16:16:02 -0500

    Added back in i18n support.

---
+++ javascript.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', function() {
+  function hideMenus(except) { document.querySelectorAll('menu.active').forEach(function(e) { if (e != except) e.classList.remove('active'); }) }
+  function setActive(id, pushState) {
+    hideMenus();
+    document.querySelectorAll('pages page.active').forEach(function(p) { p.classList.remove('active'); });
+    (document.querySelector("#page-" + (id || "index")) || document.querySelector('#page-404')).classList.add('active');
+    document.querySelector('content').className = id || "index";
+    document.title = "Lite XL" + (id && id != "index" ? " - " + document.querySelector('page.active h1').textContent : "");
+    if (pushState) 
+      history.pushState({ id: id }, document.title, window.location.pathname + (id && id != "index" ? "?/" + id.replace(/\-/,"/") : ""));
+  }
+  document.querySelectorAll('menu').forEach(function(e) { e.addEventListener('click', function(ev) { hideMenus(e); ev.stopPropagation(); e.classList.toggle('active'); }) })
+  document.querySelector('expander').addEventListener('click', function(e) { document.querySelector('links').classList.toggle('active'); })
+  document.querySelector('body').addEventListener('click', hideMenus);
+  window.addEventListener('popstate', function(e) { setActive(e.state.id, false); });
+  document.querySelectorAll('a[href^="http"], a[href^="//"], a[href^="img"]').forEach(function(a) { a.setAttribute('target', "_blank"); });
+  document.querySelectorAll('a[href^="/"]').forEach(function(a) { a.addEventListener('click', function(ev) {
+    ev.stopPropagation(); ev.preventDefault();
+    setActive(a.getAttribute('href').toLowerCase().replace(/(^\/(\w{2})|\/$)/, "").replace(/\//g, "-").replace(/^-/, ""), true);
+  })});
+  setActive(window.location.search.replace(/^\?\/?/, "").replace(/\/$/, "").replace(/\//g, "-") || "index", true);
+});
--- scripts/site.rb
+++ scripts/site.rb
@@ -6,9 +6,9 @@ class RedRouge < Redcarpet::Render::HTML
  end
 rc = Redcarpet::Markdown.new(RedRouge.new(with_toc_data: true), { fenced_code_blocks: true, tables: true, footnotes: true })
 sitemap = File.open("sitemap.txt", "w")
-File.write("index.html", File.read('template.html').gsub("{{ pages }}", "<style type='text/css'>" + Rouge::Themes::Base16.mode(:dark).render(scope: 'pre') + "</style>" +
-  Dir.glob("en/**/*").select { |x| File.file?(x) }.map { |x| 
-    sitemap.write("https://lite-xl.com" + (x =~ /index\.md/ ? "" : "?/" + x.downcase.gsub(/^en\//, "").gsub(/\.\w+$/, "")) + "\n")
-    "<page id='page-#{x.downcase.gsub(/(^en\/[^a-z0-9_\-\/\\]|\.\w+$)/,"").gsub(/[\s\\\/]+/, "-")}'>#{rc.render(File.read(x))}</page>" 
+Dir.glob("locales/*").map { |locale| File.write(File.basename(locale) + ".html", File.read("#{locale}/template.html").gsub("{{ pages }}", "<style type='text/css'>" + Rouge::Themes::Base16.mode(:dark).render(scope: 'pre') + "</style>" +
+  Dir.glob("#{locale}/**/*.md").select { |x| File.file?(x) }.map { |x| 
+    sitemap.write("https://lite-xl.com" + (x =~ /index\.md/ ? "" : "?/" + x.downcase.gsub(/^locales\//, "").gsub(/\.\w+$/, "")) + "\n")
+    "<page id='page-#{x.downcase.gsub(/(^locales\/#{File.basename(locale)}\/|[^a-z0-9_\-\/\\\.]|\.\w+$)/,"").gsub(/[\s\\\/]+/, "-")}'>#{rc.render(File.read(x))}</page>" 
   }.join("\n"))
-)
+) }
