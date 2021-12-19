286d09af083b054cd0204002996b76c43af98c7d
Adam Harrison <adamdharrison@gmail.com>
2021-12-19 12:52:17 -0500

    Removed fontawesome.

--- site.rb
+++ site.rb
@@ -4,13 +4,13 @@ require 'rouge'
 
 class RedRouge < Redcarpet::Render::HTML
   def block_code(code, language) "<pre>" + Rouge.highlight(code, language || "bash", 'html') + "</pre>" end
+  def image(link, title, alt_text) "<img data-src='#{link}' title='#{title}' alt='#{alt_text}'/>" end
   def link(link, title, link_content) "<a title='#{title}' #{link =~ /^(http|\/?assets)/ && "target='_blank'"} href='#{link}'>#{link_content}</a>" end
 end
 rc = Redcarpet::Markdown.new(RedRouge.new(with_toc_data: true), { fenced_code_blocks: true, tables: true, footnotes: true })
 sitemap = File.open("sitemap.txt", "w")
 sitemap.write("https://lite-xl.com");
 Dir.glob("locales/*").map { |locale| File.write(File.basename(locale) + ".html", File.read("#{locale}/template.html").gsub("{{ pages }}", 
-  "<style type='text/css'>" + Rouge::Themes::Base16.mode(:dark).render(scope: 'pre') + "</style>" +
   Dir.glob("#{locale}/**/*.md").select { |x| File.file?(x) }.map { |x| 
     sitemap.write("https://lite-xl.com/" + File.basename(locale) + (x =~ /index\.md/ ? "" : "?/" + x.downcase.gsub(/^locales\/\w+\//, "").gsub(/\.\w+$/, "")) + "\n")
     "<page id='page-#{x.downcase.gsub(/(^locales\/#{File.basename(locale)}\/|[^a-z0-9_\-\/\\\.]|\.\w+$)/,"").gsub(/[\s\\\/]+/, "-")}'>#{rc.render(File.read(x))}</page>" 
