03423b6f748e0a2fcc9dca42519f872f7918cddf
takase1121 <20792268+takase1121@users.noreply.github.com>
2022-12-22 22:08:11 +0800

    document and expand site.rb
    Untangled the sphagetti. Other than that, some configuration (via
     environment variables) for some flexibility, and also slugify
     the output filename properly.

--- site.rb
+++ site.rb
@@ -4,23 +4,93 @@ require 'rouge'
 require 'fileutils'
 
 class RedRouge < Redcarpet::Render::HTML
-  def block_code(code, language) "<pre>" + Rouge.highlight(code, language || "bash", 'html') + "</pre>" end
-  def link(link, title, link_content) "<a title='#{title}' #{link =~ /^(http|\/?assets)/ && "target='_blank'"} href='#{link}'>#{link_content}</a>" end
+  def block_code(code, language)
+    "<pre>" + Rouge.highlight(code, language || "bash", 'html') + "</pre>"
+  end
+
+  def link(link, title, link_content)
+    # make all external and asset links to open in new tab
+    "<a title='#{title}' #{link =~ /^(http|\/?assets)/ && "target='_blank'"} href='#{link}'>#{link_content}</a>"
+  end
 end
+
+def slugify(name)
+  name
+    .downcase
+    .gsub(/[^a-z0-9]+/, "-") # replace non-url friendly characters with dashes
+    .gsub(/\-+/, "-") # remove duplicate dashes
+    .gsub(/^\-|\-$/, "") # remove trailing dashes
+end
+
+
+
+# config options
+root = ENV.fetch("SITE_ROOT", "")
+domain = ENV.fetch("SITE_DOMAIN", "https://lite-xl.com")
+default_locale = ENV.fetch("SITE_LOCALE", "en")
+verbose = ENV.key?("VERBOSE")
+
+
+
+root = root.gsub(/\/\\$/, "") + "/" unless root == ""
+FileUtils.rm_rf(root) unless root == ""
 rc = Redcarpet::Markdown.new(RedRouge.new(with_toc_data: true), { fenced_code_blocks: true, tables: true, footnotes: true })
-sitemap = File.open("sitemap.txt", "w")
-sitemap.puts("https://lite-xl.com");
-Dir.glob("locales/*").map { |x| x.gsub("locales/", "") }.map { |locale| 
-  template = File.read("locales/#{locale}/template.html")
-  FileUtils.rm_rf("#{locale}")
-  Dir.glob("locales/#{locale}/**/*.md").select { |x| File.file?(x) }.map { |path|
-    target = path.gsub(/\.md/,".html").gsub(/^locales\//, "")
-    FileUtils.mkdir_p(File.dirname(target)) if !Dir.exist?(File.dirname(target))
-    contents = rc.render(File.read(path))
-    title = (contents.scan(/<\s*h1.*?>(.*?)<\s*\/h1\s*>/).first || ["Lite XL"]).first
-    title = "Lite XL - #{title}" if title != "Lite XL"
-    id = path.gsub("locales/#{locale}", "").gsub(/\.(\w+)$/, "").downcase.gsub(/[^a-z0-9]+/, "-").gsub(/\-+/, "-").gsub(/^\-|\-$/, "")
-    File.write(target, template.gsub("{{ page }}", contents).gsub("{{ title }}", title).gsub("{{ id }}", id))
-    sitemap.write("https://lite-xl.com/#{target == 'index.html' ? '' : target}\n")
+files = Dir
+  .glob("locales/*")
+  .map { |x| x.gsub("locales/", "") }
+  .map { |locale|
+    template = File.read("locales/#{locale}/template.html")
+    FileUtils.rm_rf(root + locale)
+
+    # process markdown files
+    files = Dir
+      .glob("locales/#{locale}/**/*.md")
+      .select { |x| File.file?(x) }
+      .map { |path|
+        basename = path.gsub("locales/#{locale}", "").gsub(/.\w+$/, "")
+
+        # the slugs produced by target and id is different, as target slugifies each component
+        # while id slugifies everything. For instance, path "/locale/en/magic!I don't know" produces
+        # target = /en/magic-i-don-t-know
+        # id = magic-i-don-t-know
+        target = File.join(Pathname(locale + basename).each_filename.map { |component| slugify(component) }) + ".html"
+
+        FileUtils.mkdir_p(root + File.dirname(target)) unless Dir.exist?(root + File.dirname(target))
+
+        contents = rc.render(File.read(path))
+        title = (contents.scan(/<\s*h1.*?>(.*?)<\s*\/h1\s*>/).first || ["Lite XL"]).first
+        title = "Lite XL - #{title}" unless title == "Lite XL"
+        id = slugify(basename)
+
+        contents = template
+          .gsub("{{ page }}", contents)
+          .gsub("{{ title }}", title)
+          .gsub("{{ id }}", id)
+        File.write(root + target, contents)
+
+        # return target filename for sitemap
+        target
+      }
+
+    # html file passthrough
+    files + Dir
+      .glob("locales/#{locale}/**/*.html")
+      .select { |file| file != "locales/#{locale}/template.html" }
+      .each { |file| FileUtils.copy_file(file, root + file.gsub("locales/", ""), true, true) }
   }
-}
+  .flatten
+  .each { |path| puts("#{path} generated.") if verbose }
+  .map { |path| "#{domain}/#{path == "index.html" ? '' : path}" }
+  .unshift(domain) # prepend the domain
+
+# write sitemap
+File.write("#{root}sitemap.txt", files.join("\n") + "\n")
+
+# link index.html for default locale
+FileUtils.ln_sf("#{root}#{default_locale}/index.html", "#{root}index.html")
+
+# copy other files
+unless root == ""
+  FileUtils.cp_r("assets", "#{root}assets")
+  FileUtils.cp("404.html", "#{root}404.html")
+end
