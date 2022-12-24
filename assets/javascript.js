4116fb47f8d504649700c583bf4802b0e72704c5
takase1121 <20792268+takase1121@users.noreply.github.com>
2022-12-24 20:16:40 +0800

    add site search via DDG
    client-side searching will be added in the future.

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,6 +2,6 @@ window.addEventListener('DOMContentLoaded', function() {
   function click(selector, callback) { document.querySelectorAll(selector).forEach(function(e) { e.addEventListener('click', function(ev) { callback(ev, e); }); }) }
   function hideMenus(except) { document.querySelectorAll('menu.active').forEach(function(e) { if (e != except) e.classList.remove('active'); }) }
   click('menu', function(ev, e) { hideMenus(e); ev.stopPropagation(); e.classList.toggle('active'); });
-  click('expander', function() { document.querySelector('links').classList.toggle('active'); });
+  click('expander', function() { document.querySelectorAll('links').forEach(function(e) { e.classList.toggle('active'); }); });
   click('body', hideMenus);
 });
